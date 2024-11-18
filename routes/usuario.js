78dc6df9aea650687e341863db1b18a8f987d01b
Bruno Vicente <brunnovicente@gmail.com>
2024-11-18 09:21:00 -0300

    Layout do login

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import usuarioController from '../controllers/UsuarioController.js';
 
-router.get('/login', (req, res) => res.render('usuario/login'));
+router.get('/login', (req, res) => res.render('usuario/login', {layout: 'secundario'}));
 router.post('/login', usuarioController.login);
 router.get('/logout', usuarioController.logout);
 router.get('/senha/:senha', usuarioController.senha);
