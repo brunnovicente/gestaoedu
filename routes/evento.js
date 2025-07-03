569ee9b39ee8a9ec6acb2920cb723e2de170e622
brunnovicente <brunnovicente@gmail.com>
2025-07-03 18:56:22 -0300

    Encerramento das inscrições

--- routes/evento.js
+++ routes/evento.js
@@ -4,12 +4,12 @@ import EventoController from '../controllers/EventoController.js';
 import {autorizar} from "../helpers/permissao.js";
 
 router.get('/', EventoController.index)
-router.get('/cadastro', EventoController.cadastro)
-router.post('/salvar', EventoController.salvar)
-router.get('/inscricao/:id', EventoController.inscricao)
-router.post('/salvartime', EventoController.salvartime)
-router.get('/excluirtime/:id/:aluno_id', EventoController.exlcuirtime)
-router.post('/salvarmembro', EventoController.salvarmembro)
+//router.get('/cadastro', EventoController.cadastro)
+//router.post('/salvar', EventoController.salvar)
+//router.get('/inscricao/:id', EventoController.inscricao)
+//router.post('/salvartime', EventoController.salvartime)
+//router.get('/excluirtime/:id/:aluno_id', EventoController.exlcuirtime)
+//router.post('/salvarmembro', EventoController.salvarmembro)
 
 router.get('/getAluno/:matricula', EventoController.getAluno)
 router.get('/verificarColetiva/:matricula', EventoController.verificarColetiva)
