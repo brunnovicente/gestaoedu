3aa61fba9b1e4afc0946572b5562010b1b526fc1
Bruno Vicente <brunnovicente@gmail.com>
2025-07-23 17:14:12 -0300

    Remoção do Evento da Janaina

--- routes/ingresso.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import IngressoController from "../controllers/IngressoController.js";
-import {autorizar, isLogado} from "../helpers/permissao.js"
-
-router.get('/codigo',  IngressoController.gerar)
-
-export default router;
